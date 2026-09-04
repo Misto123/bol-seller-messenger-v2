110c1e9965cfadc467eb7f9d12bcdfb27677c0cd
Bram <bram.1592@gmail.com>
2026-09-04 10:02:13 +0200

    Update layout to use blue favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Seller Messenger",
   description: "Bol seller outreach console",
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
